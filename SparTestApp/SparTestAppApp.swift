14380ce9d3f68ef674ccc64577b83ce6ebbfda8d
Boris Kravchenko <allmycalifornia@gmail.com>
2024-08-27 17:43:21 +0300

    Initial Commit

---
+++ SparTestApp/SparTestAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  SparTestAppApp.swift
+//  SparTestApp
+//
+//  Created by Борис Кравченко on 27.08.2024.
+//
+
+import SwiftUI
+
+@main
+struct SparTestAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
